5467c8d452a9c1ed77b554e638da512959e74503
deviantfero <fmorataya.04@gmail.com>
2018-02-09 04:32:16 -0600

    apply webRTC middleware

--- client/redux/store.js
+++ client/redux/store.js
@@ -3,6 +3,7 @@ import thunk from 'redux-thunk';
 import {createLogger} from 'redux-logger';
 import fileApp from './reducers/fileapp';
 import {wsMiddleware} from './middleware/wsMiddleware';
+import {webrtcMiddleware} from './middleware/webrtcMiddleware';
 
-let store = createStore(fileApp, applyMiddleware(createLogger(), thunk, wsMiddleware));
+let store = createStore(fileApp, applyMiddleware(createLogger(), thunk, wsMiddleware, webrtcMiddleware));
 export default store;
