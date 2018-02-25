0ded213435583a44ddc23124e3da7457b2d3fabf
deviantfero <fmorataya.04@gmail.com>
2018-02-25 04:24:22 -0600

    remove thunk, unnecessary middleware

--- client/redux/store.js
+++ client/redux/store.js
@@ -1,9 +1,8 @@
 import {createStore, applyMiddleware} from 'redux';
-import thunk from 'redux-thunk';
 import {createLogger} from 'redux-logger';
 import fileApp from './reducers/fileapp';
 import {wsMiddleware} from './middleware/wsMiddleware';
 import {webrtcMiddleware} from './middleware/webrtcMiddleware';
 
-let store = createStore(fileApp, applyMiddleware(createLogger(), thunk, wsMiddleware, webrtcMiddleware));
+let store = createStore(fileApp, applyMiddleware(createLogger(), wsMiddleware, webrtcMiddleware));
 export default store;
